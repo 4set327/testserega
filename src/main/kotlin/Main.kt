6d930713fe01aa2fb02711697b71db5b8cb846f7
4set327 <91342557+4set327@users.noreply.github.com>
2021-09-24 22:34:19 +0300

    Feature/trie (#1)
     принт 1 2 , 34

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,13 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
-
+    test()
     println("Hello World!")
+}
+
+fun test() {
+    println("1 2")
+    println("1 3 4")
+        println("Hello Serega!")
+    println("Hello Serega!")
 }
