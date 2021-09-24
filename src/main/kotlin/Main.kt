f42d686d8485d71a890004bf55c9065412e9b619
4set327 <4set.327@gmail.com>
2021-09-24 21:16:56 +0300

    инит

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments at Run/Debug configuration
+    println("Program arguments: ${args.joinToString()}")
+    test()
+    println("Hello World!")
+}
+
+fun test() {
+    println("Hello Serega!")
+    println("Hello Serega!")
+    println("Hello Serega!")
+}
