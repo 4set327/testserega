3c19ff6c892268488d7a8c8e3906707dd4a808c1
4set327 <4set.327@gmail.com>
2021-09-24 21:47:46 +0300

    удалил тест

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,11 +4,6 @@ fun main(args: Array<String>) {
 
     // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
-    test()
-    println("Hello World!")
-}
 
-fun test() {
-        println("Hello Serega!")
-    println("Hello Serega!")
+    println("Hello World!")
 }
